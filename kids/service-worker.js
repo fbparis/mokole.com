a9b32aa8d80a6d705357f6eb90b2bcd2883576bf
FB <fbparis@gmail.com>
2017-11-28 17:08:31 +0800

    sw 1.3

--- kids/service-worker.js
+++ kids/service-worker.js
@@ -1,8 +1,27 @@
-var version = "v1:0:2";
+var version = "v1.3::";
 
 self.addEventListener("install", function (e) {
 	console.log("worker: install event fired.");
-	return self.skipWaiting();
+	event.waitUntil(
+		caches.open(version + "mk").then(function (cache) {
+			return cache.addAll([
+				"favicon.ico",
+				"img/tinified/hand1-64.png",
+				"img/tinified/hand2-64.png",
+				"img/tinified/loading.apng",
+				"img/tinified/mokole-kids.png",
+				"img/tinified/sprite-volume.png",
+				"img/tinified/wood-bg-01.png",
+				"img/tinified/wood-bg-02.png",
+				"img/tinified/wood-bg-03.png",
+				"sound/newgame.mp3",
+				"sound/good.mp3",
+				"sound/wrong.mp3",
+				"sound/win.mp3"
+			]);
+		}).then(console.log("worker: install complete."))
+	);
+// 	return self.skipWaiting();
 });
 
 self.addEventListener("fetch", function (e) {
@@ -11,5 +30,14 @@ self.addEventListener("fetch", function (e) {
 
 self.addEventListener("activate", function (e) {
 	console.log("worker: activate event fired.");
-	return self.clients.claim();
+	event.waitUntil(
+		caches.keys().then(function (keys) {
+			return Promise.all(
+				keys.filter(function (key) { return !key.startWith(version); }).map(function (key) {
+					return caches.delete(key);
+				})
+			)
+		}).then(console.log("worker: activate completed."))
+	);
+// 	return self.clients.claim();
 });
