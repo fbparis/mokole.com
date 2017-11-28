830c0a256376ae3e953d35912f85d0faf1440993
FB <fbparis@gmail.com>
2017-11-28 13:22:07 +0800

    worker 1:0:1

--- kids/service-worker.js
+++ kids/service-worker.js
@@ -1,5 +1,13 @@
-var version = "v1::";
+var version = "v1:0:1";
 
 self.addEventListener("install", function (e) {
 	console.log("worker: install event fired.");
+});
+
+self.addEventListener("fetch", function (e) {
+	console.log("worker: fetch event fired.");
+});
+
+self.addEventListener("activate", function (e) {
+	console.log("worker: activate event fired.");
 });
