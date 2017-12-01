b00328e3eca7ac9b4ff054964c11a62165e042b2
FB <fbparis@gmail.com>
2017-12-01 13:23:26 +0800

    sw disabling offline analytics support

--- kids/service-worker.js
+++ kids/service-worker.js
@@ -55,7 +55,7 @@ function openDatabaseAndReplayRequests() {
     replayAnalyticsRequests();
   };
 }
-openDatabaseAndReplayRequests();
+//openDatabaseAndReplayRequests();
 function getObjectStore(storeName, mode) {
   return idbDatabase.transaction(storeName, mode).objectStore(storeName);
 }
@@ -105,7 +105,7 @@ self.addEventListener("fetch", function(event) {
 	if ((url.hostname === 'www.google-analytics.com' ||
 	url.hostname === 'ssl.google-analytics.com') &&
 	url.pathname.indexOf("/collect") != -1) {
-		//return;
+		return;
 		//Analytics 
 		console.log("WORKER: handling analytics request", event.request.url);
 		event.respondWith(
