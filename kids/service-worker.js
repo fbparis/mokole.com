b5a13a3348a8244d069e35b00ab5445cc2cd924a
FB <fbparis@gmail.com>
2017-12-03 13:33:06 +0800

    sw 2.12

--- kids/service-worker.js
+++ kids/service-worker.js
@@ -1,17 +1,18 @@
 "use strict";
 
-console.log('WORKER: executing.');
+console.log("WORKER: executing.");
 
-var version = 'v2.10::';
+var version = "v2.12::";
 var offline = version + "offline";
 var dynamic = version + "dynamic";
 
 // Offline analytics settings
-var idbDatabase;
+var idbDatabase = null;
+var IDB_NAME = "mokole-kid";
 var IDB_VERSION = 1;
 var STOP_RETRYING_AFTER = 86400000; // One day, in milliseconds.
-var STORE_NAME = 'urls';
-var CUSTOM_DIMENSION = 'Offline';
+var STORE_NAME = "offline-analytics";
+var CUSTOM_DIMENSION = "Offline";
 // Bypass Adsense
 var ADS_URL = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
 
@@ -41,87 +42,96 @@ var notCached = [
 ];
 
 // Offline Analytics
-function openDatabaseAndReplayRequests() {
+(function openDatabaseAndReplayRequests() {
 	console.log("ANALYTICS: starting openDatabaseAndReplayRequests()");
-  var indexedDBOpenRequest = indexedDB.open('offline-analytics', IDB_VERSION);
-  indexedDBOpenRequest.onerror = function(error) {
-    console.error('ANALYTICS: IndexedDB error:', error);
-  };
-  indexedDBOpenRequest.onupgradeneeded = function() {
-    this.result.createObjectStore(STORE_NAME, {keyPath: 'url'});
-  };
-  indexedDBOpenRequest.onsuccess = function() {
-    idbDatabase = this.result;
-    replayAnalyticsRequests();
-  };
-}
-//openDatabaseAndReplayRequests();
+	var indexedDBOpenRequest = indexedDB.open(IDB_NAME, IDB_VERSION);
+	indexedDBOpenRequest.onerror = function(error) {
+		console.error("ANALYTICS: onerror:", error);
+	};
+	indexedDBOpenRequest.onupgradeneeded = function() {
+		console.log("ANALYTICS: onupgrdeneeded", this);
+		var db = this.result;
+		if (!db.objectStoreNames.contains(STORE_NAME)) {
+			var objectStore = db.createObjectStore(STORE_NAME, {keyPath: "url"});
+			//objectStore.createIndex("timestamp", "timestamp", {unique: false});
+		}
+	};
+	indexedDBOpenRequest.onsuccess = function() {
+		console.log("ANALYTICS: onsuccess", this);
+		idbDatabase = this.result;
+		replayAnalyticsRequests();
+	};
+})();
 function getObjectStore(storeName, mode) {
-  return idbDatabase.transaction(storeName, mode).objectStore(storeName);
+	return idbDatabase.transaction(storeName, mode).objectStore(storeName);
 }
 function replayAnalyticsRequests() {
-  var savedRequests = [];
-  getObjectStore(STORE_NAME).openCursor().onsuccess = function(event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      savedRequests.push(cursor.value);
-      cursor.continue();
-    } else {
-      savedRequests.forEach(function(savedRequest) {
-        var queueTime = Date.now() - savedRequest.timestamp;
-        if (queueTime > STOP_RETRYING_AFTER) {
-          getObjectStore(STORE_NAME, 'readwrite').delete(savedRequest.url);
-          console.warn('ANALYTICS: Request has been queued for %d milliseconds. ' +
-            'No longer attempting to replay.', queueTime);
-        } else {
-          var requestUrl = savedRequest.url + '&qt=' + queueTime + '&cd1=' + CUSTOM_DIMENSION;
-          fetch(requestUrl).then(function(response) {
-            if (response.status < 400) {
-              getObjectStore(STORE_NAME, 'readwrite').delete(savedRequest.url);
-            } else {
-              console.error('ANALYTICS: Replaying failed:', response);
-            }
-          }).catch(function(error) {
-            console.error('ANALYTICS: Replaying failed:', error);
-          });
-        }
-      });
-    }
-  };
+	var savedRequests = [];
+	getObjectStore(STORE_NAME).openCursor().onsuccess = function(event) {
+		var cursor = event.target.result;
+		if (cursor) {
+			savedRequests.push(cursor.value);
+			cursor.continue();
+		} else {
+			savedRequests.forEach(function(savedRequest) {
+				var queueTime = Date.now() - savedRequest.timestamp;
+				if (queueTime > STOP_RETRYING_AFTER) {
+					getObjectStore(STORE_NAME, "readwrite").delete(savedRequest.url);
+					console.warn("ANALYTICS: Request has been queued for %d milliseconds. " +
+					"No longer attempting to replay.", queueTime);
+				} else {
+					var requestUrl = savedRequest.url + "&qt=" + queueTime + "&cd1=" + CUSTOM_DIMENSION;
+					fetch(requestUrl).then(function(response) {
+						if (response.status < 400) {
+							getObjectStore(STORE_NAME, "readwrite").delete(savedRequest.url);
+						} else {
+							console.error("ANALYTICS: Replaying failed:", response);
+						}
+					}).catch(function(error) {
+						console.error("ANALYTICS: Replaying failed:", error);
+					});
+				}
+			});
+		}
+	};
 }
 
 // Events listeners
-
 self.addEventListener("fetch", function(event) {
-	if ((event.request.method !== 'GET') || (event.request.url.indexOf("data:") == 0) || notCached.some(function(v) { return event.request.url.indexOf(v) >= 0; })) {
-		console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+	if ((event.request.method !== "GET") || (event.request.url.indexOf("data:") == 0) || notCached.some(function(v) { return event.request.url.indexOf(v) >= 0; })) {
+		console.log("WORKER: fetch event ignored.", event.request.method, event.request.url);
 		return;
 	}
 	// Adsense
 	if (event.request.url.indexOf(ADS_URL) !== -1) {
+		console.log("WORKER: fetch event ignored.", event.request.method, event.request.url);
 		return;
 	}	
 	var url = new URL(event.request.url);
-	if ((url.hostname === 'www.google-analytics.com' ||
-	url.hostname === 'ssl.google-analytics.com') &&
+	if ((url.hostname === "www.google-analytics.com" ||
+	url.hostname === "ssl.google-analytics.com") &&
 	url.pathname.indexOf("/collect") != -1) {
-		return;
 		//Analytics 
-		console.log("WORKER: handling analytics request", event.request.url);
-		event.respondWith(
-			fetch(event.request).then(function(response) {
-				if (response.status >= 400) {
-					throw Error('ANALYTICS: Error status returned from Google Analytics request.');
-				}			
-				return response;
-			}).catch(function(error) {
-				getObjectStore(STORE_NAME, 'readwrite').add({
-					url: event.request.url,
-					timestamp: Date.now()
-				});				
-				return error;
-			})
-		);
+		if (idbDatabase) {
+			console.log("WORKER: handling analytics request", event.request.url);
+			event.respondWith(
+				fetch(event.request).then(function(response) {
+					if (response.status >= 400) {
+						throw Error("ANALYTICS: Error status returned from Google Analytics request.");
+					}			
+					return response;
+				}).catch(function(error) {
+					getObjectStore(STORE_NAME, "readwrite").add({
+						url: event.request.url,
+						timestamp: Date.now()
+					});				
+					return error;
+				})
+			);			
+		} else {
+			console.log("WORKER: fetch event ignored.", event.request.method, event.request.url);
+			return;
+		}
 	} else {
 		// Else...
 		event.respondWith(
@@ -148,17 +158,17 @@ self.addEventListener("fetch", function(event) {
 				}
 				function unableToResolve() {
 					console.log("WORKER: unable to resolve", event.request.url);
-			        return new Response('<h1>Service Unavailable</h1>', {
+			        return new Response("<h1>Service Unavailable</h1>", {
 						status: 503,
-						statusText: 'Service Unavailable',
+						statusText: "Service Unavailable",
 						headers: new Headers({
-							'Content-Type': 'text/html'
+							"Content-Type": "text/html"
 						})
 					});
 				}
 				return cache.match(event.request, {ignoreSearch: true}).then(function(cachedOffline) {
 					if (cachedOffline) {
-						console.log('WORKER: fetch event [offline cached]', event.request.url);
+						console.log("WORKER: fetch event [offline cached]", event.request.url);
 						if (event.request.url.indexOf("?") == -1) {
 							fetch(event.request).then(fetchedOfflineFromNetwork, unableToResolve);				
 						}
@@ -170,7 +180,7 @@ self.addEventListener("fetch", function(event) {
 								if (!cachedDynamic) {
 									return unableToResolve();
 								}
-								console.log('WORKER: fetch event [dynamic cached]', event.request.url);
+								console.log("WORKER: fetch event [dynamic cached]", event.request.url);
 								return cachedDynamic;
 							});
 						});
@@ -182,7 +192,7 @@ self.addEventListener("fetch", function(event) {
 });
 
 self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
+  console.log("WORKER: install event in progress.");
   event.waitUntil(
     caches
       .open(offline)
@@ -190,14 +200,14 @@ self.addEventListener("install", function(event) {
         return cache.addAll(cachedFirst);
       })
       .then(function() {
-        console.log('WORKER: install completed');
+        console.log("WORKER: install completed");
         return self.skipWaiting();
       })
   );
 });
 
 self.addEventListener("activate", function(event) {
-  console.log('WORKER: activate event in progress.');
+  console.log("WORKER: activate event in progress.");
   event.waitUntil(
     caches
       .keys()
@@ -213,7 +223,7 @@ self.addEventListener("activate", function(event) {
         );
       })
       .then(function() {
-        console.log('WORKER: activate completed.');
+        console.log("WORKER: activate completed.");
         return self.clients.claim();
       })
   );
